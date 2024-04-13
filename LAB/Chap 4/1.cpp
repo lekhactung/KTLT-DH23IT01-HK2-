3af8646091fa3d4155dce1db88d9de69a2b6ec81
lekhactung <tungle07102005@gmail.com>
2024-04-13 18:40:18 +0700

    upd cstring

--- LAB/Chap 4/1.cpp
+++ LAB/Chap 4/1.cpp
@@ -1,48 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void soSanh (string a, string b){
-    int length1  = a.length();
-    int length2 = b.length();
-    if(length1>length2) {
-        cout << a << "dai hon "<<b ;
-    } else{
-        cout << b << "dai hon "<< a;
-    }
-}
-
-void upper1stChar (string a,string b){
-    bool is1stchar = true;
-    for(char &kytu :a){
-        if(is1stchar && !isspace(kytu)){
-            kytu=toupper(kytu);
-            is1stchar = false;
-        }
-        else if(isspace(kytu)){
-            is1stchar=true;
-        }
-    }
-    is1stchar=true;
-    for(char &kytu :b){
-        if(is1stchar && !isspace(kytu)){
-            kytu=toupper(kytu);
-            is1stchar = false;
-        }
-        else if(isspace(kytu)){
-            is1stchar=true;
-        }
-    }
-    cout << a << " " << b;
-}
-string attach (string a,string b){
-    a+=b;
-    return a;
-}
 
 int main(){
-    string a,b;
-    getline(cin,a);
-    getline(cin,b);
-     upper1stChar(a,b);
-    return 0 ;
+
+    return 0;
 }
