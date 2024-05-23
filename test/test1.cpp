a42d748c9446b4b203e2f71f81e192be6a919f49
lekhactung <tungle07102005@gmail.com>
2024-05-23 09:12:52 +0700

    upd

--- test/test1.cpp
+++ test/test1.cpp
@@ -1,46 +1,57 @@
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
+#include <bits/stdc++.h>
 using namespace std;
-const int MAX_R = 50, MAX_C=50;
-void Input(int arr[][MAX_C],int r, int c){
-    for(int i=0;i<r;i++){
-        for(int j=0;j<c;j++){
-            // arr[i][j]= rand()%10;
-            cin >> arr[i][j];
-        }
 
-    }
-}
-void Output(int arr[][MAX_C],int r, int c){
-    for(int i=0;i<r;i++){
-        for(int j=0;j<c;j++){
-            cout << arr[i][j] << " ";
-        }
-        cout << endl;
+int dectobin(int n){
+    if (n<2){
+        return n;
+    } else {
+        int tmp = n%2;
+        return dectobin(n/2) *10 + tmp;
     }
 }
 
+int bintodec(int n,int i=0){
+    if(n==0){
+        return 0;
+    } else{
+        int tmp = n%10;
+        return tmp * pow(2,i)+bintodec(n/10,i+1);
+    }
+}
+
+void swap(int &a,int &b){
+    int tmp = a;
+    a=b;
+    b=tmp;
+}
+
+void sort(int a[],int n){
+    if(n==1){
+        return;
+    } else{
+        sort(a,n-1);
+        if(a[n-2]>a[n-1]){
+            swap(a[n-2],a[n-1]);
+            return sort(a,n-1);
+        }
+    }
+}
 int main(){
-    srand(time(NULL));
-    int arr[MAX_R][MAX_C]={{1,2,3,4},{6,7,8,9}};
-    int r=2 ,c=4 ;
-    // cin >> r >> c;
-    // Input(arr,r,c);
-    // cout << "//////" << endl;
-    Output(arr,r,c);
-    int cnt =0;
-    for(int i=0;i<r;i++){
-        for(int j=0;j<c;j++){
-            for(int k=0;k<r;k++){
-                for(int l=0;l<c;l++){
-                    if(arr[i][j]+arr[k][l]==10 && (i!=k && j!=l)){
-                        cnt++;
-                    }
-                    } 
-                }
-            }
-        }
-    cout << cnt/2;
+    // cout << dectobin(10);
+    // cout << bintodec(1010);
+    int a[10];
+    for(int i=0;i<10;i++){
+        a[i]=rand()%10 +1;
+    }
+    
+     for(int i=0;i<10;i++){
+        cout << a[i]<< " ";
+    }
+    cout << endl;
+    sort(a,10);
+     for(int i=0;i<10;i++){
+        cout << a[i]<< " ";
+    }
+
     return 0;
 }
