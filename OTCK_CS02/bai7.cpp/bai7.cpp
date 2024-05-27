073fb4be1f1bbf5a350afc8cf1978749b521dc87
lekhactung <tungle07102005@gmail.com>
2024-05-27 21:29:37 +0700

    upd

---
+++ OTCK_CS02/bai7.cpp/bai7.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+class Tamgiac{
+private:
+    int a,b,c;
+public:
+    int geta(){
+        return a;
+    }
+    int getb(){
+        return b;
+    }
+    int getc(){
+        return c;
+    }
+};
+
+int main(){
+
+
+    return 0;
+}
